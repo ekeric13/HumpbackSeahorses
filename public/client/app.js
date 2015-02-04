e3c29535dbd72d31502ee5fa2e8dcd60f0ad2abe
Eric Kennedy <ekeric13@gmail.com>
2015-02-03 18:18:39 -0800

    Fix merge conflicts in app.js and index.html

--- public/client/app.js
+++ public/client/app.js
@@ -3,6 +3,6 @@ var socket = io();
 
 $(function(){
   var messages = new Messages();
-  new SubmitView({el: $('.submitView'), collection: messages});
+g  new SubmitView({el: $('.submitView'), collection: messages});
   new MessagesView({el: $('#messagesView'), collection: messages});
 });
